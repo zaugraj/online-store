fb5897bde5f711ee8ed0bc5c55eef264a4d21a11
zaugraj <jillz29@msn.com>
2013-11-13 18:46:26 -0500

    created includes

---
+++ _footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+	<p>Copyright 2013 My Online Store</p>
+</div>
---
+++ _header.php
@@ -0,0 +1,8 @@
+<div id="header">
+	<h1 id="logo">My Online Store</h1>
+    <ul id="navigation">
+    	<li><a href="index.php">Home</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="add.php">Add a Product</a></li>
+    </ul>
+</div>
